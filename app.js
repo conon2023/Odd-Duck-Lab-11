7ae817d11461a086425992021360096170b55729
CHIDI ONONYE <fononye2020@gmail.com>
2023-06-29 15:07:17 +0100

    updated code to ensure vvoting stops after 25 rounds. Also displated the winning product and the number of times seen

--- app.js
+++ app.js
@@ -1,73 +1,115 @@
-    'use strict';
+'use strict';
 
-    const productContainer = document.getElementById('productContainer');
-    const productImages = ["bag", "banana", "bathroom", "boots", "breakfast", "bubblegum", "chair", "cthulhu", "dog-duck","dragon", "pen", "pet-sweep", "scissors", "shark", "sweep", "tauntaun", "unicorn", "water-can", "wine-glass"]
+const productContainer = document.getElementById('productContainer');
+const productImages = ["bag", "banana", "bathroom", "boots", "breakfast", "bubblegum", "chair", "cthulhu", "dog-duck","dragon", "pen", "pet-sweep", "scissors", "shark", "sweep", "tauntaun", "unicorn", "water-can", "wine-glass"];
 
-    // Constructor function for creating a product object
-    function Product(name, imagePath) {
-      this.name = name;
-      this.imagePath = imagePath;
-      this.timesShown = 0;
-    }
-      // Create product objects
+let numRounds = 25; // Default number of rounds
+
+// Constructor function for creating a product object
+function Product(name, imagePath) {
+  this.name = name;
+  this.imagePath = imagePath;
+  this.timesShown = 0;
+  this.votes = 0;
+}
+
+// Create product objects
 const products = [
-    new Product("bag", "images/bag.jpg"),
-    new Product("banana", "images/banana.jpg"),
-    new Product("bathroom", "images/bathroom.jpg"),
-    new Product("boots", "images/boots.jpg"),
-    new Product("breakfast", "images/breakfast.jpg"),
-    new Product("bubblegum", "images/bubblegum.jpg"),
-    new Product("chair", "images/chair.jpg"),
-    new Product("cthulhu", "images/cthulhu.jpg"),
-    new Product("dog-duck", "images/dog-duck.jpg"),
-    new Product("dragon", "images/dragon.jpg"),
-    new Product("pen", "images/pen.jpg"),
-    new Product("pet-sweep", "images/pet-sweep.jpg"),
-    new Product("scissors", "images/scissors.jpg"),
-    new Product("shark", "images/shark.jpg"),
-    new Product("sweep", "images/sweep.png"),
-    new Product("tauntaun", "images/tauntaun.jpg"),
-    new Product("unicorn", "images/unicorn.jpg"),
-    new Product("water-can", "images/water-can.jpg"),
-    new Product("wine-glass", "images/wine-glass.jpg")
-  ];
+  new Product("bag", "images/bag.jpg"),
+  new Product("banana", "images/banana.jpg"),
+  new Product("bathroom", "images/bathroom.jpg"),
+  new Product("boots", "images/boots.jpg"),
+  new Product("breakfast", "images/breakfast.jpg"),
+  new Product("bubblegum", "images/bubblegum.jpg"),
+  new Product("chair", "images/chair.jpg"),
+  new Product("cthulhu", "images/cthulhu.jpg"),
+  new Product("dog-duck", "images/dog-duck.jpg"),
+  new Product("dragon", "images/dragon.jpg"),
+  new Product("pen", "images/pen.jpg"),
+  new Product("pet-sweep", "images/pet-sweep.jpg"),
+  new Product("scissors", "images/scissors.jpg"),
+  new Product("shark", "images/shark.jpg"),
+  new Product("sweep", "images/sweep.png"),
+  new Product("tauntaun", "images/tauntaun.jpg"),
+  new Product("unicorn", "images/unicorn.jpg"),
+  new Product("water-can", "images/water-can.jpg"),
+  new Product("wine-glass", "images/wine-glass.jpg")
+];
 
-
-
-    // Function to get three unique random product indices
+// Function to get three unique random product indices
 function getRandomProductIndices() {
-    const indices = [];
-    while (indices.length < 3) {
-      const randomIndex = Math.floor(Math.random() * productImages.length);
-      if (!indices.includes(randomIndex)) {
-        indices.push(randomIndex);
-      }
+  const indices = [];
+  while (indices.length < 3) {
+    const randomIndex = Math.floor(Math.random() * productImages.length);
+    if (!indices.includes(randomIndex)) {
+      indices.push(randomIndex);
     }
-    return indices;
+  }
+  return indices;
+}
+
+// Function to display three random products
+function displayRandomProducts() {
+  if (numRounds === 0) {
+    // End the voting session
+    productContainer.removeEventListener('click', productClickHandler);
+    const viewResultsButton = document.createElement('button');
+    viewResultsButton.textContent = 'View Results';
+    viewResultsButton.addEventListener('click', showResults);
+    document.body.appendChild(viewResultsButton);
+    return;
   }
 
-    // Function to display three random products
-    function displayRandomProducts() {
-      const productIndices = getRandomProductIndices();
+  const productIndices = getRandomProductIndices();
 
-      for (let i = 0; i < productIndices.length; i++) {
-        const productIndex = productIndices[i];
-        const product = products[productIndex];
-        const imageElement = document.getElementById(`product${i + 1}`);
-        imageElement.src = product.imagePath;
-        imageElement.alt = product.name;
-        product.timesShown++;
-      }
-    }
+  for (let i = 0; i < productIndices.length; i++) {
+    const productIndex = productIndices[i];
+    const product = products[productIndex];
+    const imageElement = document.getElementById(`product${i + 1}`);
+    imageElement.src = product.imagePath;
+    imageElement.alt = product.name;
+    product.timesShown++;
+  }
 
-    // Event listener for product click
-    function productClickHandler(event) {
-      // Generate three new random products
-      displayRandomProducts();
-    }
+  numRounds--;
+}
 
-    // Attach event listener to product container
-    productContainer.addEventListener('click', productClickHandler);
+// Event listener for product click
+function productClickHandler(event) {
+  const clickedProductId = event.target.id;
+  if (clickedProductId === 'product1' || clickedProductId === 'product2' || clickedProductId === 'product3') {
+    const clickedProductIndex = parseInt(clickedProductId.replace('product', '')) - 1;
+    const clickedProduct = products[clickedProductIndex];
+    clickedProduct.votes++;
+  }
 
-    // Display initial random products
-    displayRandomProducts();
+  // Generate three new random products
+  displayRandomProducts();
+}
+
+// Event listener for "View Results" button click
+function showResults() {
+  // Remove the "View Results" button
+  this.remove();
+
+  // Sort the products based on votes received
+  const sortedProducts = products.sort((a, b) => b.votes - a.votes);
+
+  // Create a report of results
+  const resultsContainer = document.createElement('div');
+  resultsContainer.classList.add('results-container');
+
+  sortedProducts.forEach((product) => {
+    const productResult = document.createElement('p');
+    productResult.textContent = `${product.name} had ${product.votes} vote${product.votes !== 1 ? 's' : ''}, and was seen ${product.timesShown} time${product.timesShown !== 1 ? 's' : ''}.`;
+    resultsContainer.appendChild(productResult);
+  });
+
+  document.body.appendChild(resultsContainer);
+}
+
+// Attach event listener to product container
+productContainer.addEventListener('click', productClickHandler);
+
+// Display initial random products
+displayRandomProducts();
